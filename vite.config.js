e3ab995df61b77d3f48a5fed976538a8d64ed215
최주은 <ilutoo@naver.com>
2025-04-11 14:55:55 +0900

    설정 변경

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base:"https://lovestardev.github.io/jueun/",
     plugins: [react()],
     server: {
         port: 3000, // 개발 서버 포트 설정
