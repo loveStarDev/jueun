7dcf584ce79d110dbb846e401384a5ba09b756f8
최주은 <ilutoo@naver.com>
2025-04-15 15:39:30 +0900

    Certification, Sideprojects 추가

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,12 +1,14 @@
 import Myself from "./ui/Myself";
 import Experience from "./ui/Experience"
 import Certification from './ui/Certification'
+import SideProjects from './ui/SideProjects';
 import './About.css';
 
 function About() { 
     return <div className="container">
         <Myself></Myself>
         <Experience></Experience>
+        <SideProjects></SideProjects>
         <Certification></Certification>
     </div>;
 }
