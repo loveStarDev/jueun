d121d308e9575193f8d2a51bdf4525885a73eec0
최주은 <ilutoo@naver.com>
2025-04-16 10:56:56 +0900

    Bottom 추가, CSS 변경

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,20 @@
 import Myself from "./ui/Myself";
-import Experience from "./ui/Experience"
-import Certification from './ui/Certification'
-import SideProjects from './ui/SideProjects';
-import './About.css';
+import Experience from "./ui/Experience";
+import Certification from "./ui/Certification";
+import SideProjects from "./ui/SideProjects";
+import Bottom from "./ui/Bottom";
+import "./About.css";
 
-function About() { 
-    return <div className="container">
-        <Myself></Myself>
-        <Experience></Experience>
-        <SideProjects></SideProjects>
-        <Certification></Certification>
-    </div>;
+function About() {
+    return (
+        <div className="container">
+            <Myself />
+            <Experience />
+            <SideProjects />
+            <Certification />
+            <Bottom />
+        </div>
+    );
 }
 
 export default About;
